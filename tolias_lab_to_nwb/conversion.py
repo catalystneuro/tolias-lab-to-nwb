9b196a698a8712896f27f46de0c62b992745e5b8
!git for-each-ref --format='%(refname:short)' `git symbolic-ref HEAD` <ben.dichter@gmail.com>
2020-01-01 21:46:58 -0800

    add command line support

--- tolias_lab_to_nwb/conversion.py
+++ tolias_lab_to_nwb/conversion.py
@@ -1,75 +1,112 @@
+import argparse
 import os
 import uuid
 
 import numpy as np
-from data_prep import data_preparation
 from dateutil import parser
 from pynwb import NWBFile, NWBHDF5IO
+from pynwb.file import Subject
 from pynwb.icephys import CurrentClampStimulusSeries, CurrentClampSeries, IZeroClampSeries
+from ruamel import yaml
 from scipy.io import loadmat
 
-fpath='/Users/bendichter/data/Berens/08 01 2019 sample 1.mat'
+from .data_prep import data_preparation
 
-fpath_base, fname = os.path.split(fpath)
-session_id = os.path.splitext(fname)[0]
-data = loadmat(fpath)
 
-session_date = parser.parse(os.path.split(fpath)[1][:10])
+# fpath = '/Users/bendichter/data/Berens/08 01 2019 sample 1.mat'
 
-time, current, voltage, curr_index_0 = data_preparation(data)
 
-nwbfile = NWBFile('Current clamp square',
-                  str(uuid.uuid4()),
-                  session_date,
-                  experimenter='Federico Scala',
-                  lab='Tolias',
-                  institution='Baylor College of Medicine',
-                  experiment_description='Current clamp square',
-                  session_id=session_id)
+def gen_current_stim_template(times, rate):
+    current_template = np.zeros((int(rate * times[-1]),))
+    current_template[int(rate * times[0]):int(rate * times[1])] = 1
 
-device = nwbfile.create_device(name='Heka ITC-1600')
+    return current_template
 
-elec = nwbfile.create_ic_electrode(name="elec0",
-                                   description='an intracellular electrode',
-                                   device=device)
 
-times = [.1, .7, .9]
-rate = 25e3
-current_template = np.zeros((int(rate * times[-1]),))
-current_template[int(rate * times[0]):int(rate * times[1])] = 1
+def make_nwb(current, voltage, rate, session_id, metadata, filepath):
 
-for i, (ivoltage, icurrent) in enumerate(zip(voltage.T, current)):
+    session_date = parser.parse(session_id[:10])
 
-    if icurrent == 0:
-        nwbfile.add_acquisition(IZeroClampSeries(
-            name="CurrentClampSeries{:03d}".format(i),
-            data=ivoltage,
-            electrode=elec,
-            rate=rate,
-            gain=1.,
-            sweep_number=i))
-    else:
-        nwbfile.add_stimulus(CurrentClampStimulusSeries(
-            name="CurrentClampStimulusSeries{:03d}".format(i),
-            data=current_template * icurrent,
-            starting_time=np.nan,
-            rate=rate,
-            electrode=elec,
-            gain=1.,
-            sweep_number=i))
+    nwbfile_args = dict(
+        identifier=str(uuid.uuid4()),
+        session_start_time=session_date,
+        session_id=session_id
+    )
+    nwbfile_args.update(metadata['NWBFile'])
+    nwbfile = NWBFile(**nwbfile_args)
 
-        nwbfile.add_acquisition(CurrentClampSeries(
-            name="CurrentClampSeries{:03d}".format(i),
-            data=ivoltage,
-            electrode=elec,
-            rate=rate,
-            gain=1.,
-            sweep_number=i))
+    nwbfile.subject = Subject(**metadata['Subject'])
 
-out_fpath = session_id + '.nwb'
-with NWBHDF5IO(out_fpath, 'w') as io:
-    io.write(nwbfile)
+    device = nwbfile.create_device(**metadata['Icephys']['Device'])
 
-#  test read
-with NWBHDF5IO(out_fpath, 'r') as io:
-    io.read()
+    elec = nwbfile.create_ic_electrode(device=device,
+                                       **metadata['Icephys']['Electrode'])
+
+    current_template = gen_current_stim_template(times=(.1, .7, .9), rate=rate)
+
+    for i, (ivoltage, icurrent) in enumerate(zip(voltage.T, current)):
+
+        if icurrent == 0:
+            nwbfile.add_acquisition(IZeroClampSeries(
+                name="CurrentClampSeries{:03d}".format(i),
+                data=ivoltage,
+                electrode=elec,
+                rate=rate,
+                gain=1.,
+                sweep_number=i))
+        else:
+            nwbfile.add_stimulus(CurrentClampStimulusSeries(
+                name="CurrentClampStimulusSeries{:03d}".format(i),
+                data=current_template * icurrent,
+                starting_time=np.nan,
+                rate=rate,
+                electrode=elec,
+                gain=1.,
+                sweep_number=i))
+
+            nwbfile.add_acquisition(CurrentClampSeries(
+                name="CurrentClampSeries{:03d}".format(i),
+                data=ivoltage,
+                electrode=elec,
+                rate=rate,
+                gain=1.,
+                sweep_number=i))
+
+    with NWBHDF5IO(filepath, 'w') as io:
+        io.write(nwbfile)
+
+    #  test read
+    with NWBHDF5IO(filepath, 'r') as io:
+        io.read()
+
+
+def main():
+    argparser = argparse.ArgumentParser(description='convert .mat file to NWB')
+    argparser.add_argument("input_fpath", type=str, help="path of .mat file to convert")
+    argparser.add_argument("-o", "--output_fpath", type=str, help="path to save NWB file",
+                           default=None)
+    argparser.add_argument("-m", "--metafile", type=str, help="YAML file that contains meta-data for experiment",
+                           default=None)
+
+    args = argparser.parse_args()
+
+    fpath_base, fname = os.path.split(args.input_fpath)
+    session_id = os.path.splitext(fname)[0]
+    data = loadmat(args.input_fpath)
+
+    time, current, voltage, curr_index_0 = data_preparation(data)
+
+    if not args.output_fpath:
+        args.output_fpath = os.path.join(fpath_base, session_id + '.nwb')
+
+    if not args.metafile:
+        args.metafile = os.path.join(fpath_base, 'metafile.yml')
+
+    with open(args.metafile) as f:
+        metadata = yaml.safe_load(f)
+
+    make_nwb(current, voltage, 25e3, session_id, metadata, args.output_fpath)
+
+
+if __name__ == "__main__":
+    main()
